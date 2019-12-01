45c887ff6a45a9d290c9ac9125dbf525810b7d91
DaniGadget18 <dani_181298@hotmail.com>
2019-12-01 17:28:50 -0600

    Que onda

--- Jugador.py
+++ Jugador.py
@@ -11,6 +11,43 @@ class Jugador:
         self.color = color
     
     def moverPosicion(self):
-        pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
-        
+
+        if self.posicionRnd == 5:
+            self.posicionRnd + 31
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 9:
+            self.posicionRnd + 42
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 36:
+            self.posicionRnd - 32
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 23:
+            self.posicionRnd + 20
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 49:
+            self.posicionRnd - 42
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 56:
+            self.posicionRnd - 48
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 48:
+            self.posicionRnd + 38
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 62:
+            self.posicionRnd + 21
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 69:
+            self.posicionRnd + 22
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 87:
+            self.posicionRnd - 21
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 82:
+            self.posicionRnd - 62
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        elif self.posicionRnd == 95:
+            self.posicionRnd - 57
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
+        else:
+            pygame.draw.circle(self.pantalla,self.color,[self.posiciones[self.posicionRnd][0],self.posiciones[self.posicionRnd][1]],20,0)
         return self.posicionRnd
